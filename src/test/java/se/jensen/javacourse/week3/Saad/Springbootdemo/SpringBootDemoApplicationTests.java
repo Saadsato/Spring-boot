70774c18f8c6d2633369c60f2216929ccdb0b8fc
Saad Shafe <saaabd010401@student.jenseneducation.se>
2022-12-05 19:44:24 +0100

    Added some files.

---
+++ src/test/java/se/jensen/javacourse/week3/Saad/Springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jensen.javacourse.week3.Saad.Springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
